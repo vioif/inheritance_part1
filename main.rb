dccad2e448f611a8ff3096b93b4b9c00efb9d6cd
vioif <ireneaflores3@gmail.com>
2017-12-21 16:36:42 -0500

    final commits

--- main.rb
+++ main.rb
@@ -6,3 +6,16 @@ chris = Student.new('Chris')
 
 p nadia.greeting
 p chris.greeting
+
+nadia.teach
+chris.learn
+
+puts nadia.teach
+puts chris.learn
+
+chris.teach
+
+puts chris.teach
+
+
+# teach method on student instance doesn't work b/c it's an undefined method on the student
